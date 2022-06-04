4a95b44adc67a89ed9a5e9406c7eb4c7a1da7754
김재욱 <kjwook7522@gmail.com>
2022-06-05 03:16:25 +0900

    Feat: connect mysql using ORM sequelize

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import { Sequelize } from "sequelize";
 
+const sequelize = new Sequelize("ace_card", "kjwook7522", "3552", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+async function connect() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
 const app = express();
 const port = 8000;
 
@@ -9,4 +23,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  connect();
 });
